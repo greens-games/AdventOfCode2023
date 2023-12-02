2be39ba6d87f28875199c1c28fb429038c6ebc60
Matthew <mgreen72194@gmail.com>
2023-12-02 01:06:34 -0600

    testing done

--- zig_impl/day_1/src/main.zig
+++ zig_impl/day_1/src/main.zig
@@ -2,30 +2,48 @@ const std = @import("std");
 const root = @import("root");
 const ArrayList = std.ArrayList;
 const testing = std.testing;
+const allocator = std.heap.page_allocator;
 
 pub fn main() !void {
     const input = [_][]const u8{ "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" };
 
-    for (input) |c| {
-        const curr_len: usize = c.len;
-        _ = curr_len;
-        std.debug.print("{c}", .{c[0]});
+    var sum: u8 = 0;
+
+    for (input) |s| {
+        const first_digit: u8 = try find_first_digit(s);
+        const second_digit: u8 = try find_last_digit(s);
+        const digits_concate = try std.fmt.allocPrint(allocator, "{d}{d}", .{ first_digit, second_digit });
+
+        sum += try std.fmt.parseInt(u8, digits_concate, 10);
     }
+    std.debug.print("SUM IS: {}\n", .{sum});
 }
 
-fn find_first_digit(s: []const u8) u8 {
+fn find_first_digit(s: []const u8) !u8 {
     for (s) |char| {
-        _ = char;
+        if (try is_digit(char))
+            return char - '0';
     }
+
+    return 11;
 }
 
-fn find_last_digit(s: []const u8) u8 {
-    _ = s;
+fn find_last_digit(s: []const u8) !u8 {
+    var length: usize = s.len - 1;
+
+    while (length >= 0) {
+        const char: u8 = s[length];
+        if (try is_digit(char))
+            return char - '0';
+        length -= 1;
+    }
+
+    return 11;
 }
 
 fn is_digit(c: u8) !bool {
-    const allocator = std.heap.page_allocator;
     var list = ArrayList(u8).init(allocator);
+    defer list.deinit();
 
     try list.append('0');
     try list.append('1');
@@ -40,11 +58,9 @@ fn is_digit(c: u8) !bool {
 
     for (list.allocatedSlice()) |item| {
         if (c == item) {
-            list.deinit();
             return true;
         }
     }
-    list.deinit();
     return false;
 }
 
@@ -58,4 +74,16 @@ test "is_digit test return true" {
 test "is_digit test return false" {
     const result: bool = try is_digit('a');
     try testing.expectEqual(false, result);
+}
+
+test "find_first_digit return 1" {
+    const s = "ab1";
+    const result: u8 = try find_first_digit(s);
+    try testing.expectEqual(@as(u8, 1), result);
+}
+
+test "find_last_digit return 2" {
+    const s = "1b2";
+    const result: u8 = try find_last_digit(s);
+    try testing.expectEqual(@as(u8, 2), result);
 }
